3b741bfefea7162e24cb1da3cbf0dc830f7573b9
Pisti1304 <72573537332@szily.hu>
2025-03-31 08:46:39 +0200

    kesz

--- feltolt.php
+++ feltolt.php
@@ -6,7 +6,7 @@ $jelszo = "ujjelszo123";
 $hash = password_hash($jelszo, PASSWORD_BCRYPT);
 
 $dbvez = new DBVezerlo() ;
-$query = "UPDATE adin SET passwd = ? WHERE fnev = ?" ;
+$query = "UPDATE admin SET passwd = ? WHERE fnev = ?" ;
 
 if($dbvez->executeQuery($query,[$hash,$fnev],"ss")){
     echo "Admin jelszava frissítve";
--- includes/dbvezerlo.php
+++ includes/dbvezerlo.php
@@ -1,26 +1,25 @@
 <?php
-
-class DBVezerlo{
+class DBVezerlo {
     private $conn;
-    private $host =  "localhost";
-
+    private $host = "localhost";
     private $user = "root";
-
     private $password = "";
-
     private $database = "carad";
 
-    function __construct(){
-        $this->conn = new mysqli($this->host, $this->user, $this->password);
-        if($this->conn->connect_error){ 
-            die("Kapcsolódási hiba". $this->conn->connect_error);
+    function __construct() {
+        $this->conn = new mysqli($this->host,
+        $this->user, $this->password, $this->database);
+        if ($this->conn->connect_error) {
+            die("Kapcsolódási hiba: " . $this->conn->connect_error);
         }
+        $this->conn->set_charset("utf8");
     }
 
-    function executSelectQuery($query, $params =[],$types = ""){
-        $stmt= $this->conn->prepare($query);
-        if(!empty($params)){
-            $stmt->bind_param($types, $params);
+    // SELECT lekérdezésekhez
+    function executeSelectQuery($query, $params = [], $types = "") {
+        $stmt = $this->conn->prepare($query);
+        if (!empty($params)) {
+            $stmt->bind_param($types, ...$params);
         }
         $stmt->execute();
         $result = $stmt->get_result();
@@ -28,26 +27,31 @@ class DBVezerlo{
         $stmt->close();
         return $resultset;
     }
-    function executeQuery($query, $params = [],$types = ""){
-        $stmt= $this->conn->prepare($query);
-        if(!empty($params)){
-            $stmt->bind_param($types, $params);
+
+    // INSERT, UPDATE, DELETE lekérdezésekhez
+    function executeQuery($query, $params = [], $types = "") {
+        $stmt = $this->conn->prepare($query);
+        if (!empty($params)) {
+            $stmt->bind_param($types, ...$params);
         }
-        $result = $stmt->execute();
+        $result = $stmt->execute(); // Végrehajtja a lekérdezést
         $stmt->close();
         return $result;
     }
 
-    public function getLastInsertId(){
+    //utolsó beszúrt id
+    public function getLastInsertId() {
+        return $this->conn->insert_id; // Ha a conn a mysqli kapcsolat
+    }
+
+    public function getInsertId() {
         return $this->conn->insert_id;
     }
-    public function getInsertId(){
-        return $this->conn->insert_id;
-    }
-    public function closeDB()
-    {
-        $this->conn->close();
+
+    function closeDB() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
     }
 }
 ?>
-
